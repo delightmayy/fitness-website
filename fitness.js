636598dff3b0c760d72399493e3217fc55d4a6cb
delightedmayy <delightedmayy@gmail.com>
2024-02-11 19:43:53 +0100

    fitness site update 1
    adding herosection and section 2

---
+++ fitness.js
@@ -0,0 +1,16 @@
+let person1 = document.querySelector("#optt");
+let person2 = document.querySelector("#optt2");
+let person3 = document.querySelector("#optt3");
+console.log(person1);
+person1.addEventListener("mouseover", ()=>{person1.style  ="background-image: url(/images/personaltrainer3-pic1.webp);    background-position: center;background-size: cover;height: 450px;"
+})
+person1.addEventListener("mouseout", ()=>{person1.style  ="background-image: url(/images/personaltrainer3-pic1a.webp);    background-position: center;background-size: cover;height: 450px;"
+})
+person2.addEventListener("mouseover", ()=>{person2.style  ="background-image: url(/images/personaltrainer3-pic2.webp);    background-position: center;background-size: cover;height: 450px;"
+})
+person2.addEventListener("mouseout", ()=>{person2.style  ="background-image: url(/images/personaltrainer3-pic2a.webp);    background-position: center;background-size: cover;height: 450px;"
+})
+person3.addEventListener("mouseover", ()=>{person3.style  ="background-image: url(/images/personaltrainer3-pic3.webp);    background-position: center;background-size: cover;height: 450px;"
+})
+person3.addEventListener("mouseout", ()=>{person3.style  ="background-image: url(/images/personaltrainer3-pic3a.webp);    background-position: center;background-size: cover;height: 450px;"
+})
