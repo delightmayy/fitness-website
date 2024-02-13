da007eb04ea5cde27cff502097ae0ca079b3920a
delightedmayy <delightedmayy@gmail.com>
2024-02-13 14:06:52 +0100

    site completed and open to correction

--- fitness.js
+++ fitness.js
@@ -1,6 +1,9 @@
-let person1 = document.querySelector("#optt");
-let person2 = document.querySelector("#optt2");
-let person3 = document.querySelector("#optt3");
+const person1 = document.querySelector("#optt");
+const person2 = document.querySelector("#optt2");
+const person3 = document.querySelector("#optt3");
+const mydate =document.querySelector(".mydate");
+const btt = document.querySelector(".btt");
+
 
 person1.addEventListener("mouseover", ()=>{person1.style  ="background-image: url(/images/personaltrainer3-pic1.webp);    background-position: center;background-size: cover;height: 450px;"
 })
@@ -14,3 +17,11 @@ person3.addEventListener("mouseover", ()=>{person3.style  ="background-image: ur
 })
 person3.addEventListener("mouseout", ()=>{person3.style  ="background-image: url(/images/personaltrainer3-pic3a.webp);    background-position: center;background-size: cover;height: 450px;"
 })
+btt.addEventListener("mouseover", ()=>{btt.style  ="background-color:brown; text:black ;"
+})
+
+ updateyear()
+ function updateyear () {
+    mydate.innerHTML = new Date().toLocaleDateString()
+    
+} 
